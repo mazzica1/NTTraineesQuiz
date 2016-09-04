3f04b2ba859eaea66aee25f005911ac93f2de2fb
Mzamir <engsamir2017@gmail.com>
2016-09-04 17:00:00 +0200

    create class for comment

--- app/src/main/java/nt_me/nttraineesquiz/ItemDetailFragment.java
+++ app/src/main/java/nt_me/nttraineesquiz/ItemDetailFragment.java
@@ -1,15 +1,6 @@
 package nt_me.nttraineesquiz;
 
-import android.app.Activity;
-import android.support.design.widget.CollapsingToolbarLayout;
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-import nt_me.nttraineesquiz.dummy.DummyContent;
 
 /**
  * A fragment representing a single Item detail screen.
